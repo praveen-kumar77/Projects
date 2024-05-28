33dae76ebcc7c5f0cf66db0f000208e1d9144e6b
praveen <pkoffice2003@gmail.com>
2024-05-28 16:42:07 +0530

    test base

---
+++ CommonFactor.java
@@ -0,0 +1,30 @@
+package com.java;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class CommonFactor {
+    public static void main(String[] args) {
+        String word1 = "TAUXXTAUXXTAUXXTAUXXTAUXX";
+        String word2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
+        String check1 = "";
+        String check2 = "";
+    }
+}
+/*StringBuilder answer = new StringBuilder();
+        Set<Character> set1 = new LinkedHashSet<>();
+        Set<Character> set2 = new LinkedHashSet<>();
+        for(char i : word1.toCharArray()){
+            set1.add(i);
+        }
+        for(char i : word2.toCharArray()){
+            set2.add(i);
+        }
+        if(set1.equals(set2)){
+            for(char i : set2){
+                answer.append(i);
+            }
+        }
+        System.out.println(answer);*/
