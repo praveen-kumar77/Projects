a4cb7d54c6063642fe326b8dcca9db54c8c4e537
praveen <pkoffice2003@gmail.com>
2024-05-28 16:45:54 +0530

    test case

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {23, 45, 12, 21, 56, 32, 55, 3};
+        int n = arr.length - 1;
+        int temp;
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
