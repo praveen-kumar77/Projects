e5004b1e844fac9635d103f4fb63e497c66761bc
praveen <pkoffice2003@gmail.com>
2024-06-23 18:37:11 +0530

    added

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,10 +5,9 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {23, 45, 12, 21, 56, 32, 55, 3};
-        int n = arr.length - 1;
         int temp;
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= n - 1; j++){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length - 1; j++){
                 if(arr[j] > arr[j + 1]){
                     temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -16,6 +15,6 @@ public class BubbleSort {
                 }
             }
         }
-        System.out.println(Arrays.toString(arr) + "j");
+        System.out.println(Arrays.toString(arr));
     }
 }
--- CommonFactor.java
+++ CommonFactor.java
@@ -9,13 +9,10 @@ public class CommonFactor {
     public static void main(String[] args) {
         String word1 = "TAUXXTAUXXTAUXXTAUXXTAUXX";
         String word2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
-        String check1 = "";
-        String check2 = "";
-    }
-}
-/*StringBuilder answer = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         Set<Character> set1 = new LinkedHashSet<>();
         Set<Character> set2 = new LinkedHashSet<>();
+
         for(char i : word1.toCharArray()){
             set1.add(i);
         }
@@ -27,4 +24,6 @@ public class CommonFactor {
                 answer.append(i);
             }
         }
-        System.out.println(answer);*/
+        System.out.println(answer);
+    }
+}
